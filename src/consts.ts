f71bccf8621e00aeef8b5fd08b52780d7a7ce547
O-Isaac <zaragozamendozaisaac@gmail.com>
2024-01-17 21:29:08 +0100

    feat: New title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SiteTitle = 'Edge UI';
+export const SiteTitle = 'Edge UI - Patchs Notes';
 export const SiteDescription = 'Welcome to my changelogs sites!';
 export const SiteDefaultImage = 'https://beta-edge-ui-docs.vercel.app/assets/images/minified/hero-bg.webp'
