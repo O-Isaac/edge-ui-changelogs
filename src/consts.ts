73f805d523f0e0c49a149ab8ef089f31ebe6dac5
O-Isaac <zaragozamendozaisaac@gmail.com>
2024-01-17 21:26:47 +0100

    feat: Add default image metatags

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SiteTitle = 'Edge UI';
-export const SiteDescription = 'Welcome to my website!';
+export const SiteDescription = 'Welcome to my changelogs sites!';
+export const SiteDefaultImage = 'https://beta-edge-ui-docs.vercel.app/assets/images/minified/hero-bg.webp'
