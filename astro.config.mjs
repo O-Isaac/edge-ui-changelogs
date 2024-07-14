5243b554ad40e31dcb1c41b9789d4212a309a573
O-Isaac <soporte@proyectograndorder.es>
2024-07-14 18:52:40 +0200

    Add adapter vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
-import keystatic from '@keystatic/astro'
+import keystatic from '@keystatic/astro';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://changelogs-edge-ui.vercel.app',
   output: 'hybrid',
-  integrations: [react(), markdoc(), keystatic()]
+  integrations: [react(), markdoc(), keystatic()],
+  adapter: vercel()
 });
